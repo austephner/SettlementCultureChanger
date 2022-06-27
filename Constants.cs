be8e85b19e407a67ab5027860fb929798f65ba00
Austin <austephner@gmail.com>
2022-06-27 18:17:55 -0400

    moved files up directory

---
+++ Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace SettlementCultureChanger
+{
+    public static class Constants
+    {
+        public const string DISPLAY_VERSION = "0.0.1";
+    }
+}
---
+++ PerSaveModSettings.cs
@@ -0,0 +1,38 @@
+﻿using MCM.Abstractions.Attributes;
+using MCM.Abstractions.Attributes.v2;
+using MCM.Abstractions.Dropdown;
+using MCM.Abstractions.Settings.Base.PerSave;
+
+namespace SettlementCultureChanger
+{
+    public class PerSaveModSettings : AttributePerSaveSettings<PerSaveModSettings>
+    {
+        public override string Id => "SettlementCultureChanger";
+        
+        public override string DisplayName => $"Settlement Culture Changer ({Constants.DISPLAY_VERSION})";
+
+        #region GENERAL 
+        
+        [SettingPropertyGroup("General")]
+        [SettingPropertyBool("Enabled")]
+        public bool enabled { get; set; } = true;
+
+        #endregion
+
+        #region Automatic Conversion
+
+        [SettingPropertyGroup("Automatic Conversion")]
+        [SettingPropertyDropdown("Mode", HintText = "Determines how a settlement's culture is automatically converted over time.")]
+        public DropdownDefault<string> automaticConversionMode { get; set; } = new DropdownDefault<string>(
+            new string[]
+            {
+                "(Default) Use Governor's Culture, Fallback to Owner's Culture",
+                "Governor's Culture Only",
+                "Owner's Culture Only",
+                "Disabled"
+            },
+            0);
+
+        #endregion
+    }
+}
